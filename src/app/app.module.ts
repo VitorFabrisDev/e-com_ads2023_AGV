d1fdfeeeaaf39ffaba91d84c4016f68c30949741
VitorFabrisDev <vitor2fabris@hotmail.com>
2023-06-15 20:46:03 -0300

    AJUSTE DO HTML DE PRODUTOS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgSelectModule } from '@ng-select/ng-select';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ListaProdutosComponent } from './lista-produtos/lista-produtos.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaProdutosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgSelectModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
